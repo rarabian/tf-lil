92bc01d0db080d027c87497578b577f81e72a6f6
Rick Arabian <rick@Ricks-FatMac.kgbird.com>
2019-11-27 13:07:54 -0800

    Ready to build servers.

--- connections.tf
+++ connections.tf
@@ -3,13 +3,14 @@ provider "google" {
     project = "rick-a"
     region = "us-central1"
 }
+
 provider "aws" {
     region = "us-west-2"
 }
 
-provider "azurerm" {
-    subscription_id = "0"
-    client_id = "1"
-    client_secret = "2"
-    tennant_id = "3"
-}
+#provider "azurerm" {
+#    subscription_id = "456739a9-5a6f-48e0-867c-15a5e3750d34"
+#    client_id = "1"
+#    client_secret = "2"
+#    tennant_id = "45d659f4-1555-498b-93b5-0b99a89a284b"
+#}
--- resources.tf
+++ resources.tf
@@ -1,13 +1,44 @@
 resource "google_compute_network" "our_development_network" {
     name = "devnetwork"
-    auto_create_subnetworks = true
+    auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "dev-subnet" {
+    ip_cidr_range = "10.0.1.0/24"
+    name = "devsubnet"
+    network = "${google_compute_network.our_development_network.self_link}"
+    region = "us-central1"
+}
 resource "aws_vpc" "environment-example-too" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
         Name = "terraform_aws_example_too"
+    }
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${cidrsubnet(aws_vpc.environment-example-too.cidr_block, 3, 1)}"
+    vpc_id = "${aws_vpc.environment-example-too.id}"
+    availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+    cidr_block = "${cidrsubnet(aws_vpc.environment-example-too.cidr_block, 2, 2)}"
+    vpc_id = "${aws_vpc.environment-example-too.id}"
+    availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = "${aws_vpc.environment-example-too.id}"
+
+    ingress {
+        cidr_blocks = [
+            "${aws_vpc.environment-example-too.cidr_block}"   
+        ]
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
     }
 }
